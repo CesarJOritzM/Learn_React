6a4354c6fa8d6340e7ea517d931562de4ace2571
Cesar Javier Ortiz Montero <cesarjavierortizmontero@gmail.com>
2021-02-15 15:18:23 -0500

    implementacion de hooks

--- PlatziConf - Hooks/src/Routes/App.jsx
+++ PlatziConf - Hooks/src/Routes/App.jsx
@@ -7,21 +7,26 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
+  const InitialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={InitialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
--- PlatziConf - Hooks/src/containers/Home.jsx
+++ PlatziConf - Hooks/src/containers/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Products from '../components/Products'
-import initialState from '../../initialState'
+import Products from '../components/Products';
+import initialState from '../initialState';
 
-const Home = () => {
-  return (
-    <Products products={initialState.products} />
-  );
-};
+const Home = () => <Products products={initialState.products} />;
 
 export default Home;
